e6fa52d8b7dfcfe5b08a7709829012c94647856a
adina-ciobanu-selab <adina.ciobanu@mta.ro>
2020-10-27 10:41:59 +0200

    Adaugare proiect biblioteca
    Am adaugat sursele java ce tin de proiectul cu biblioteca pentru a putea fi urmarit de git
    Signed-off-by: adina-ciobanu-selab <adina.ciobanu@mta.ro>

---
+++ src/ro/mta/facc/selab/Main.java
@@ -0,0 +1,42 @@
+package ro.mta.facc.selab;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Carte c = new Carte("Casa Buddenbrook", "Thomas Mann", 1999, 500);
+        Biblioteca bib = new Biblioteca();
+        bib.addCarte(c);
+        bib.addCarte(new Carte("Codul lui DaVinci", "dan brown", 12, 23));
+        bib.addCarte(new Carte("Codul li", "danwn", 2, 3));
+        bib.sort();
+        System.out.println(bib);
+
+    */
+        Thread thread1 = new MyThread();
+        Thread thread2 = new MyThread();
+        Thread thread3 = new MyThread();
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            thread3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
